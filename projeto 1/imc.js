f473a120849693c452582a2dac42ba7bd2c2fb02
NathanRodrigues59 <nathanrodriguesvieira99@gmail.com>
2024-06-24 11:20:53 -0300

    projetos js

---
+++ projeto 1/imc.js
@@ -0,0 +1,43 @@
+
+const calcular = document.getElementById('calcular');
+
+
+
+
+
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        const valorImc = (peso / (altura * altura)).toFixed(1)
+
+        let classificacao = '';
+        if (valorImc < 18.5) {
+            classificacao = 'abaixo  do peso'
+        } else if (valorImc < 25) {
+            classificacao = 'com peso ideal. Parabéns'
+        } else if (valorImc < 30) {
+            classificacao = 'você está levemente acima do peso'
+        } else if (valorImc < 35) {
+            classificacao = ' você esstá com obesidade grau I'
+        } else if (valorImc < 40) {
+            classificacao = 'você está com obesidade grau II'
+        } else {
+            classificacao = 'você está com obesidade grau III. Cuidado!'
+        }
+
+
+
+
+        resultado.textContent = `${nome} seu IMC é ${valorImc} e você está ${classificacao}`
+
+    } else {
+        resultado.textContent = 'Preencha todos os campos'
+    }
+}
+
+
+calcular.addEventListener('click', imc);
